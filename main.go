6b68eaf3791666b66edcdcbce8245ad7dd762a4b
Yiğit Sadıç <yigitsadic@gmail.com>
2021-05-05 20:52:15 +0300

    genel yapı toparlandı.

--- main.go
+++ main.go
@@ -65,24 +65,15 @@ func main() {
 	a := guard.Authentication{Secret: token}
 
 	r := chi.NewRouter()
+
 	r.Use(middleware.Logger)
-	r.Use(a.Guard)
 
-	r.Post("/api/certificate_verification", func(writer http.ResponseWriter, request *http.Request) {
-		/*
-			b := struct {
-					QRCode string `json:"qr_code"`
-				}{}
+	r.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+	})
 
-				err = ctx.BodyParser(&b)
-				if err != nil {
-					return err
-				}
-
-				res, err := s.QueryInStore(b.QRCode)
-		*/
-
-		json.NewEncoder(writer).Encode(store.QueryResult{
+	r.With(a.Guard).Post("/api/certificate_verification", func(writer http.ResponseWriter, request *http.Request) {
+		errResponse := store.QueryResult{
 			Status:               "not_verified",
 			MaskedFirstName:      "",
 			MaskedLastName:       "",
@@ -90,7 +81,25 @@ func main() {
 			CertificateName:      "",
 			CertificateCreatedAt: "",
 			LastUpdated:          time.Now(),
-		})
+		}
+		writer.Header().Set("Content-Type", "application/json")
+		b := struct {
+			QRCode string `json:"qr_code"`
+		}{}
+
+		err = json.NewDecoder(request.Body).Decode(&b)
+		if err != nil {
+			json.NewEncoder(writer).Encode(errResponse)
+			return
+		}
+
+		res, err := s.QueryInStore(b.QRCode)
+		if err != nil {
+			json.NewEncoder(writer).Encode(errResponse)
+			return
+		}
+
+		json.NewEncoder(writer).Encode(res)
 	})
 
 	listenAddr := net.JoinHostPort(addr, port)
