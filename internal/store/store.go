ace9a4d6ae4040d7bb954bef52056a2c31022f4a
Yiğit Sadıç <yigitsadic@gmail.com>
2021-05-05 10:56:45 +0300

    store ve testi eklendi.

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,1 +1,34 @@
 package store
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+var (
+	QRCodeNotFoundErr = errors.New("QR Code not found")
+)
+
+type QueryResult struct {
+	MaskedFirstName string    `json:"masked_first_name"`
+	MaskedLastName  string    `json:"masked_last_name"`
+	QRCode          string    `json:"qr_code"`
+	LastUpdated     time.Time `json:"last_updated"`
+}
+
+type Store struct {
+	QueryResults map[string]*QueryResult
+	Mu           *sync.Mutex
+}
+
+// Reads given QR code from store.
+func (s *Store) QueryInStore(qr_code string) (*QueryResult, error) {
+	result, ok := s.QueryResults[qr_code]
+
+	if ok {
+		return result, nil
+	} else {
+		return nil, QRCodeNotFoundErr
+	}
+}
--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -1,1 +1,62 @@
 package store
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestStore_QueryInStore(t *testing.T) {
+	s := &Store{
+		QueryResults: make(map[string]*QueryResult),
+		Mu:           &sync.Mutex{},
+	}
+
+	r1 := QueryResult{
+		MaskedFirstName: "Yi***",
+		MaskedLastName:  "Sa***",
+		QRCode:          "ygt",
+		LastUpdated:     time.Now(),
+	}
+
+	r2 := QueryResult{
+		MaskedFirstName: "Ay***",
+		MaskedLastName:  "Ço***",
+		QRCode:          "aycn",
+		LastUpdated:     time.Now(),
+	}
+
+	s.QueryResults[r1.QRCode] = &r1
+	s.QueryResults[r2.QRCode] = &r2
+
+	t.Run("it should return if finds", func(a *testing.T) {
+		got, err := s.QueryInStore(r1.QRCode)
+		if err != nil {
+			t.Errorf("unexpected to get an error but got=%s", err)
+		}
+
+		if got.MaskedFirstName != r1.MaskedFirstName {
+			t.Errorf("expected return not satisfied for masked first name")
+		}
+
+		if got.MaskedLastName != r1.MaskedLastName {
+			t.Errorf("expected return not satisfied for masked last name")
+		}
+
+		if got.QRCode != r1.QRCode {
+			t.Errorf("expected return not satisfied for QR code")
+		}
+	})
+
+	t.Run("it should give error if cannot find", func(t *testing.T) {
+		got, err := s.QueryInStore("ABCD")
+
+		if got != nil {
+			t.Errorf("unexpected to find")
+		}
+
+		if err != QRCodeNotFoundErr {
+			t.Errorf("expected error was=%s but got=%s", QRCodeNotFoundErr, err)
+		}
+	})
+}
